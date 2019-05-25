347c2afaaf3f9ac3539722aee89405ec02670af7
Wynn Chaney <wynn.chaney@wanco.com>
2019-05-25 10:13:20 -0600

    Created API call for giphy.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,25 @@
+      // make connection with gighpy api
+      // pull ajax with desired animal by button push
+      // it should populate the page with 10 static images
+      // when user clicks on giphy image the gif should animate
+      // when clicked on again gif should stop playing--this will requiere if animate = true then
+      // under every gif display it rating(PG, G, so on)
+      // function that takes the input from the form and recreates the array of buttons on the page including the user input
+      // populate page with animal buttons with an array named topics
+
+      
+      
+      
       // Example queryURL for Giphy API
-      var myKey = "kxL82faz2wGCgqAlBP03ycTxEB7zLHhe"
-      var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=" + myKey;
+      var myKey = "rsnLAx7xzaoPLyiRDj394wOOJ3D3w6ul";
+      var animalTopic = "Eagle";
+
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animalTopic + "&limit=10&api_key=" + myKey;
 
 
       // <button class="btn btn-primary" type="submit">Gif Button Name Here</button>
 
-  alert("it is connected");
+  
       $.ajax({
         url: queryURL,
         method: "GET"
